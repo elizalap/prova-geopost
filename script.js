66df78474fcdd615fca5bf1e466ec615a1659dea
elizalap <elizapaoliello@outlook.com>
2022-10-10 13:41:06 -0300

    :sparkle: Exibindo times na tela

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-fetch('https://estagio.geopostenergy.com/WorldCup/GetAllTeams', {
-   method: 'GET',
-   headers: { "git-user": "elizalap" }
-}).then(response => {
-   console.log(response.json());
-});
+const url = "https://estagio.geopostenergy.com/WorldCup/GetAllTeams";
+const allTeamsContainer = document.querySelector("#all-teams-container");
+
+function shuffleArray(array) {
+   array.sort(() => Math.random() - 0.5);
+}
+
+async function getAllTeams() {
+
+   const response = await fetch(url, {
+      method: 'GET',
+      headers: { "git-user": "elizalap" }
+   });
+
+   const data = await response.json();
+   const teams = data.Result;
+
+   shuffleArray(teams);
+
+   teams.map((team) => {
+
+
+      const teamContainer = document.createElement("div");
+      const name = document.createElement("h3");
+      const token = document.createElement("p");
+
+      name.innerText = team.Name;
+      token.innerText = team.Token;
+
+      teamContainer.appendChild(name);
+      teamContainer.appendChild(token);
+
+      allTeamsContainer.appendChild(teamContainer);
+   });
+
+}
+
+// function printRandomTeams() {
+
+//    let teams = getAllTeams();
+
+//    shuffleArray(teams);
+
+//    teams.map((team) => {
+//       const div = document.createElement("div");
+//       const name = document.createElement("h3");
+//       const token = document.createElement("p");
+
+//       name.innerText = team.Name;
+//       token.innerText = team.Token;
+
+//       div.appendChild(name);
+//       div.appendChild(token);
+
+//       teamsContainer.appendChild(div);
+//    });
+
+// }
+
+getAllTeams();
+//printRandomTeams();
